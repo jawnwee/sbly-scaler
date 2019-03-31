a51635bfc2bad641837701a7b15cf99e52540a76
John <jy.johnlee@gmail.com>
2019-03-30 17:43:01 -0700

    api comments

--- api/APIClient.js
+++ api/APIClient.js
@@ -10,18 +10,22 @@ export class APIClient {
   constructor() {
   }
 
-  getTest() {
-    return axios.get('https://api.tvmaze.com/search/shows?q=batman')
-  }
-
   getAllInsights() {
-      const url = `${API_URL}/ad-insights`;
-      return axios.get(url, {
-        params: {
-          date: '2019-01-25',
-          metrics: 'spend',
-        }
-      }).then(response => response.data);
+    /*
+    Because the api is constrained to a specific date range, the api client is 
+    "dumb" here and will just make the request for all date ranges.
+    If I had more time, the data wouldn't calculated in real time, we would constantly be
+    updating our db with new values so that this amount of fetching isn't required
+    */
+    const url = `${API_URL}/ad-insights`;
+    const dates = ['2019-01-25', '2019-01-26', '2019-01-27','2019-01-28', '2019-01-29', '2019-01-30', '2019-01-31']
+    const allMetrics = 'spend,impressions,revenue,clicks'
+    return axios.get(url, {
+      params: {
+        date: '2019-01-25',
+        metrics: allMetrics,
+      }
+    }).then(response => response.data);
   }
 
 }
